1c9863fc001b64ee2e333e8d92081c58fa24e725
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-09-27 15:16:32 +0300

    navbar added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form action="">
+        <input required type="text" placeholder="username" />
+        <input required type="email" placeholder="email" />
+        <input required type="password" placeholder="password" />
+        <button>Login</button>
+        <p>this is error</p>
+        <span>
+          Do you have a account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
