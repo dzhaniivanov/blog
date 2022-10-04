866c153b6bad289080c79c1440b33ae6314983e5
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-10-04 13:43:10 +0300

    implement login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //check existing user
@@ -25,6 +26,31 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  //check user
+  const q = "SELECT * FROM users WHERE username=?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found");
+
+    //check pass
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
 
 export const logout = (req, res) => {};
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,53 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  return <div className="auth">
-    <h1>Login</h1>
-    <form action="">
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-      <button>Login</button>
-      <p>this is error</p>
-      <span>Don't you have a account? <Link to="/register">Register</Link></span>
-    </form>
-  </div>;
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [err, setErr] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/login", inputs);
+      navigate("/");
+    } catch (error) {
+      setErr(error.response.data);
+    }
+  };
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form action="">
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
+        <span>
+          Don't you have a account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
