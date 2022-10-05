c8a49134dbfef2bab3c567a225990b64b26aca8f
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-10-05 11:16:01 +0300

    delete functionality added

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,6 +1,6 @@
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -11,6 +11,7 @@ import { AuthContext } from "../context/authContexxt";
 const Single = () => {
   const [post, setPost] = useState({});
   const location = useLocation();
+  const navigate = useNavigate();
 
   const postId = location.pathname.split("/")[2];
 
@@ -28,15 +29,21 @@ const Single = () => {
 
     fetchData();
   }, [postId]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="single">
       <div className="content">
         <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
+          {post.userImg && <img src={post.userImg} alt="user avatar" />}
           <div className="info">
             <span>{post.username}</span>
             <p>Posted {moment(post.date).fromNow()}</p>
@@ -46,7 +53,7 @@ const Single = () => {
               <Link to={`/write?edit=2`}>
                 <img src={Edit} alt="" />
               </Link>
-              <img src={Delete} alt="" />
+              <img onClick={handleDelete} src={Delete} alt="" />
             </div>
           )}
         </div>
